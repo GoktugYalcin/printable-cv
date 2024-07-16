3e7556444b6009302ac17f4b05167bac940a4280
GoktugYalcin <goktugyalcin06@gmail.com>
2024-07-16 15:28:15 +0300

    fix: Some fixes

--- components/shared/PrintButton.tsx
+++ components/shared/PrintButton.tsx
@@ -8,7 +8,7 @@ const PrintButton = () => {
   return (
     <CoolMode>
       <button
-        className="fixed right-12 bottom-12 font-bold print:opacity-0 inline-flex items-center justify-center rounded-md text-lg transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2"
+        className="fixed active:scale-95 right-12 bottom-12 font-bold print:opacity-0 inline-flex items-center justify-center rounded-md text-lg transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2"
         onClick={handlePrint}
       >
         Print It!
